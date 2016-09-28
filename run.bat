8f469a4b412eff690ae12e451db7237e0239f40f
Jaime Lopez <jailop@gmail.com>
2016-09-28 08:30:53 -0600

    Minor changes

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
+set FLASK_DEBUG=0
 set FLASK_APP=alac.py
-set FLASK_DEBUG=1
 flask run --host=0.0.0.0
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/tcsh
 
-setenv FLASK_DEBUG 1
+setenv FLASK_DEBUG 0
 setenv FLASK_APP alac.py
 flask run --host=0.0.0.0
