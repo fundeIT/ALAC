aaa12fdc5cb43b0fa12cd281f47833e2b7c11cb3
Jaime Lopez <jailop@gmail.com>
2017-12-05 10:22:28 -0600

    Minor changes

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ app.secret_key = trust.secret_key
 app.config['UPLOAD_FOLDER'] = trust.docs_path
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-ALLOWED_EXTENSIONS = set(['pdf', 'docx', 'xlsx', 'jpg', 'pptx', 'txt'])
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'docx', 'xlsx', 'jpg', 'pptx', 'txt'])
 
 def uploadFile(docfile):
     docfile.filename = secure_filename(docfile.filename)
