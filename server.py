ffd33348ef1e03a4b9af762378ef2b80d1e0a323
Jaime Lopez <jailop@gmail.com>
2018-01-25 06:30:13 -0600

    Log statistics

--- server.py
+++ server.py
@@ -123,7 +123,7 @@ def hasRight(source, source_id, categories):
 @app.before_request
 def before_request():
     f = open('log.txt', 'a')
-    line = str(datetime.datetime.now()) + ' ' + request.remote_addr + ' ' + request.path + '\n'
+    line = str(datetime.datetime.now()) + ' ' + request.remote_addr + ' ' + request.path + ' ' + str(request.accept_languages) + '\n'
     f.write(line)
     f.close()
 
