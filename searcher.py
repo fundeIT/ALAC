4f62b6d8a25ea09c97aeffe0e7704e0e3cb66ba3
Jaime Lopez <jailop@gmail.com>
2018-09-12 17:18:07 -0600

    Font update

--- searcher.py
+++ searcher.py
@@ -1,47 +1,63 @@
-from whoosh.index import create_in
-from whoosh.fields import *
+from whoosh.index import create_in, open_dir
+from whoosh.fields import Schema, TEXT, ID 
 from whoosh.qparser import QueryParser
 import models
 
-class Searcher:
-    def get_schema(self):
-        return Schema(
-            title=TEXT(stored=True), 
-            path=ID(stored=True), 
-            kind=TEXT(stored=True),
-            date=TEXT(stored=True), 
-            content=TEXT
+DIR = 'indexdir'
+
+def get_schema():
+    return Schema(
+        title=TEXT(stored=True), 
+        path=ID(stored=True), 
+        kind=TEXT(stored=True),
+        date=TEXT(stored=True), 
+        content=TEXT
+    )
+
+def update_requests():
+    ix = create_in(DIR, get_schema())
+    writer = ix.writer()
+    req = models.Requests()
+    for item in req.list():
+        item = req.get(item['_id'])
+        writer.add_document(
+                title = item['overview'],
+                path = str(item['_id']),
+                kind = 'request',
+                date = item['date'],
+                content = item['detail']
         )
-    def update_requests(self):
-        ix = create_in("indexdir", self.get_schema())
-        writer = ix.writer()
-        req = models.Requests()
-        for item in req.list():
-            item = req.get(item['_id'])
-            writer.add_document(
-                    title = item['overview'],
-                    path = str(item['_id']),
-                    kind = 'request',
-                    date = item['date'],
-                    content = item['detail']
-            )
-        writer.commit()
-    def test(self):
-        ix = create_in("indexdir", schema)
-        writer = ix.writer()
-        writer.add_document(title='First document', path='/a', date='2018-09-05', content='This is the first document')
-        writer.add_document(title='Second document', path='/b', date='2018-09-05', content='This is the second document')
-        writer.commit()
-    def search(self, qry):
-        ix = create_in("indexdir", self.get_schema())
-        with ix.searcher() as searcher:
-            query = QueryParser('content', ix.schema).parse(qry)
-            res = searcher.search(query)
-            if res:
-                print(res[0])
-            else:
-                print('No results')
+    writer.commit()
+
+def update_complains():
+    ix = create_in(DIR, get_schema())
+    writer = ix.writer()
+    print(writer)
+    req = models.Complains()
+    for item in req.list():
+        item = req.get(item['_id'])
+        print(item)
+        writer.add_document(
+                title = item['overview'],
+                path = str(item['_id']),
+                kind = 'complain',
+                date = item['date'],
+                content = item['detail']
+        )
+    writer.commit()
+
+def search(qry):
+    ix = open_dir(DIR)
+    print(ix.schema)
+    with ix.searcher() as searcher:
+        query = QueryParser('content', ix.schema).parse(qry)
+        print(query)
+        res = searcher.search(query)
+        if res:
+            print(res[0])
+        else:
+            print('No results')
 
 
 if __name__ == '__main__':
-    Searcher().search('listado')
+    search('This')
