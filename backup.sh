17f84aa4897c73cba7d5fed3d2ede3cbd8c6fa5f
Jaime Lopez <jailop@gmail.com>
2019-08-29 11:54:44 -0600

    Backup script added

--- backup.sh
+++ backup.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+#
 # Backup - ALAC app
 #
 # This script makes a backup from the server where the ALAC
@@ -8,16 +10,21 @@
 # Contributors:
 #   Jaime Lopez <jailop AT protonmail DOT com>
 
-# Makinig a dump from the database
-ssh root@alac.funde.org "mongodump --db alac"
-# Generating a compressed file of the database dump
-ssh root@alac.funde.org "tar czf dump.tar.gz dump"
-# Copying the database compressed file to the local computer
-scp root@alac.funde.org:dump.tar.gz .
-# Copying files from the server to the local computer
-rsync -av --delete root@alac.funde.org:/files .
-# Updating searching indexes in the server
-ssh root@alac.funde.org "cd ALAC && python searcher.py"
-ssh root@alac.funde.org "cd ALAC && python ticketsearcher.py"
-ssh root@alac.funde.org "cd ALAC && python iaipupdate.py"
+SERVER="root@alac.funde.org"
 
+# Makinig a dump from the database
+ssh $SERVER "mongodump --db alac"
+
+# Generating a compressed file of the database dump
+ssh $SERVER "tar czf dump.tar.gz dump"
+
+# Copying the database compressed file to the local computer
+scp $SERVER:dump.tar.gz .
+
+# Copying files from the server to the local computer
+rsync -av --delete $SERVER:/files .
+
+# Updating searching indexes in the server
+ssh $SERVER "cd ALAC && python searcher.py"
+ssh $SERVER "cd ALAC && python ticketsearcher.py"
+ssh $SERVER "cd ALAC && python iaipupdate.py"
