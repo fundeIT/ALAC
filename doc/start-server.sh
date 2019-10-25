2d2f425afb2c3d0e821d80ac77a467a028eb1cff
Jaime Lopez <jailop@protonmail.com>
2019-10-25 11:14:58 -0400

    Guide to start the server

---
+++ doc/start-server.sh
@@ -0,0 +1,18 @@
+Connect to the server through SSH:
+
+    $ ssh root@alac.funde.org
+
+Start the tmux utility:
+
+    $ tmux
+
+Go to the working directory and run the application
+
+    $ cd ALAC
+    $ python tmpsrv.py
+
+Deattach the terminal using the keyboard sequence: `Ctrl+b d`
+
+Close the SSH session:
+
+    $ exit
