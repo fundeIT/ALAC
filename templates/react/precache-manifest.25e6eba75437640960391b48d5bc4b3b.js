6be716b8c6bd199f4151d327ef8532bbe6da3513
Jaime Lopez <jailop@gmail.com>
2021-10-01 14:01:21 -0400

    Test

---
+++ templates/react/precache-manifest.25e6eba75437640960391b48d5bc4b3b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db85cd9ed29ba0c0de0946c0068888e7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "47a027ff04cefbe877ec",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "655018225bb634c34dd4",
+    "url": "./static/css/main.f47b5019.chunk.css"
+  },
+  {
+    "revision": "47a027ff04cefbe877ec",
+    "url": "./static/js/2.0e5f2628.chunk.js"
+  },
+  {
+    "revision": "cec9a0aaf215407e35ee285de0acf1d6",
+    "url": "./static/js/2.0e5f2628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655018225bb634c34dd4",
+    "url": "./static/js/main.4f740b9c.chunk.js"
+  },
+  {
+    "revision": "f3f072b16ff1af6ba777",
+    "url": "./static/js/runtime-main.de55c5b9.js"
+  },
+  {
+    "revision": "fd791415ef1d308399cfe3c00f377c22",
+    "url": "./static/media/alac-logo.fd791415.png"
+  },
+  {
+    "revision": "0688192f468fbcf5a8044a04cf2327bb",
+    "url": "./static/media/document.0688192f.svg"
+  },
+  {
+    "revision": "bdcf51c54758457cb4a0bd175c8b9e20",
+    "url": "./static/media/logofunde.bdcf51c5.png"
+  },
+  {
+    "revision": "cbc1c3a3292843b50d4cf76ed1ea728a",
+    "url": "./static/media/megaphone.cbc1c3a3.svg"
+  },
+  {
+    "revision": "1e71bac71dbb669f76025968bfe92ba4",
+    "url": "./static/media/plus.1e71bac7.svg"
+  },
+  {
+    "revision": "49a27809fd5d177192931a2550cc6a32",
+    "url": "./static/media/policeman.49a27809.svg"
+  }
+]);
