69d2fc7cd6d3b5b6896d1970fff63309e6524892
Jaime Lopez <jailop@gmail.com>
2022-03-07 09:53:52 -0500

    README for monitoring

--- getmonitor.sh
+++ getmonitor.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-ssh alac@alac.funde.org "cd ALAC/monitoring && ./makereports.py 2017-04-10 2021-11-30"
+ssh alac@alac.funde.org "cd ALAC/monitoring && ./makereports.py 2017-04-10 2021-12-31"
 ssh alac@alac.funde.org "cd ALAC/monitoring && zip -r reports.zip data/ images/"
-scp alac@alac.funde.org:ALAC/monitoring/report.zip .
+scp alac@alac.funde.org:ALAC/monitoring/reports.zip .
